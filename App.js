357d0a5d909c3ffb0ae7795fb86a726de68a8ae1
Byungchan Gil <gilbc890@gmail.com>
2019-08-21 16:45:33 -0500

    todo item

--- App.js
+++ App.js
@@ -1,52 +1,84 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform,
+  ScrollView,
+  AsyncStorage
+} from "react-native";
+import { AppLoading } from "expo";
+import ToDo from "./ToDo";
+import uuidv1 from "uuid/v1";
 
-const { height, width } = Dimensions.get("window")
+const { height, width } = Dimensions.get("window");
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content"/>  
-      <Text style={styles.title}>Kawai To Do</Text>  
-      <View style={styles.card}>
-        <TextInput style={styles.input} placeholder={"New To Do"}/>
-      </View>  
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    newToDo:""
+  }
+  render() {
+    const { newToDo } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Kawai To Do</Text>
+        <View style={styles.card}>
+          <TextInput
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo}
+            onChangeText={this._controllNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView contentContainerStyles={styles.toDos}>
+            <ToDo/>
+          </ScrollView>
+
+        </View>
+      </View>
+    );
+  }
+
+  _controllNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  };
+
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F23657',
-    alignItems: 'center',
+    backgroundColor: "#F23657",
+    alignItems: "center"
   },
-  title:{
-    color:'white',
-    fontSize: 20,
+  title: {
+    color: "white",
+    fontSize: 30,
     marginTop: 50,
-    fontWeight: '200',
+    fontWeight: "200",
     marginBottom: 30
   },
-  card:{
-    backgroundColor:'white',
-    flex:1,
+  card: {
+    backgroundColor: "white",
+    flex: 1,
     width: width - 25,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
-    ...Platform.select({
-      ios:{
-        shadowColor:"rgb(50, 50, 50)",
-        shadowOpacity:0.5,
-        shadowRadius:5,
-        shadowOffset:{
-          height:-1,
-          width:0
-        }
-      },
-      android:{
-        elevation:3
-      }
-    })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25
+  },
+  toDos: {
+    alignItems: "center"
   }
 });
